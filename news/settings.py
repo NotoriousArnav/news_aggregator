bbfaa4b9b0c904affb6d8a48b4842f9073a7379d
Eliot Alderson <e.alderson@allsafe.net>
2024-04-11 17:24:00 +0530

    Added CORS Headers

--- news/settings.py
+++ news/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     # Plugins
     'rest_framework',
     'django_filters',
+    'corsheaders',
     # User Apss
     'RSS_Sources',
     'articles',
@@ -58,7 +59,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'news.urls'
 

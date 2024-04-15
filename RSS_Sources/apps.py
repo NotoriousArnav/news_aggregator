859873be21a16ef42610924e565f08fadb5be6f2
Eliot Alderson <e.alderson@allsafe.net>
2024-04-15 18:29:25 +0530

    Wtf

--- RSS_Sources/apps.py
+++ RSS_Sources/apps.py
@@ -1,6 +1,6 @@
 from django.db.utils import ProgrammingError
 from django.apps import AppConfig
-import connection
+# import connection
 
 class RssSourcesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
@@ -8,7 +8,7 @@ class RssSourcesConfig(AppConfig):
     verbose_name = "RSS Sources"
 
     def ready(self):
-        connection.close_if_unusable_or_obsolete()
+        # connection.close_if_unusable_or_obsolete()
         try:
             from jobs import updater
             updater.start()
